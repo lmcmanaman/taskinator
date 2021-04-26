a1df6ca6d52afd9ccd5a371b6db45f531b57ebee
lmcman <lesliemcmanaman@gmail.com>
2021-04-26 17:44:47 -0400

    added event function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
-var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1);
+var formEl = document.querySelector("#task-form");
 
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler); {
+formEl.addEventListener("submit", createTaskHandler); {
+         
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
